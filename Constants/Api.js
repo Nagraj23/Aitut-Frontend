95efeedb43750733eeebf7a6fec3e08954860e4c
Yogesh-Mane-07 <yogeshmane6251@gmail.com>
2026-03-31 12:48:40 +0530

    added the route with ip

--- Constants/Api.js
+++ Constants/Api.js
@@ -1,3 +1,4 @@
-export const AUTH_URL = "http://10.229.57.135:8080/api/auth";
-// export const AI_URL = "http://192.168.90.181:8000";
-// export const USER_URL = "http://192.168.90.181:3001"
+export const AUTH_URL = "http://10.66.137.26:8080/api/auth";
+// export const AUTH_URL = "http://localhost:8080/api/auth";
+// export const AI_URL = "http://localhost:8000";
+// export const USER_URL = "http://localhost:3001"
